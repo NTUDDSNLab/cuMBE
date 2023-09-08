1ff7553c489e7dd53412fd9c47f836c8145c57c3
boshow88 <boshow0122@gmail.com>
2023-09-08 19:02:02 +0800

    1. Modify the annotations of scripts in utils/ 2. Remove the cut_at() function that will not be used 3. Change the acquisition of runtime-related data in section.py to a function

--- utils/plot_variance.py
+++ utils/plot_variance.py
@@ -8,45 +8,6 @@ import matplotlib.lines as plt_lines
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def cut_at(cut_x, cut_y, cut_w, cut_h):
-    x = [cut_x - 0.50*cut_w, cut_x + 0.50*cut_w, cut_x + 0.50*cut_w, cut_x - 0.50*cut_w]
-    y = [cut_y / (cut_h**0.75), cut_y / (cut_h**0.25), cut_y * (cut_h**0.75), cut_y * (cut_h**0.25)]
-    ax.add_patch(plt_patches.Polygon(xy=list(zip(x,y)), fill=True, color='w', zorder=10))
-    ax.plot(x[  :2], y[  :2], color='black', zorder=11)
-    ax.plot(x[-2: ], y[-2: ], color='black', zorder=11)
-
-# Adjust the widths of a seaborn-generated boxplot.
-def adjust_boxplots(g, width_fac=1, shift_dist=0):
-
-    # iterating through Axes instances
-    for ax in g.axes:
-        # iterating through axes artists:
-        for c in ax.get_children():
-
-            c_type = 'Patch'   if isinstance(c, plt_patches.PathPatch) \
-                else 'Polygon' if isinstance(c, plt_patches.Polygon) \
-                else 'Line2D'  if isinstance(c, plt_lines.Line2D) \
-                else 'Other'
-
-            # searching for PathPatches and Line2Ds
-            if c_type != 'Other':
-                # getting current width of box:
-                p = c.get_path()
-                verts = p.vertices[:-1] if c_type == 'Patch' else p.vertices
-                xmin  = np.min(verts[:, 0])
-                xmax  = np.max(verts[:, 0])
-                xmid  = 0.5 * (xmin + xmax)
-                xhalf = 0.5 * (xmax - xmin)
-
-                # Patch : setting new width of box
-                # Line2D: setting new width of Ts and median lines
-                shift_x  = shift_dist if xmid - round(xmid) > 0 else -shift_dist
-                xmin_new = xmid - width_fac * xhalf + shift_x
-                xmax_new = xmid + width_fac * xhalf + shift_x
-                verts[verts[:, 0] == xmin, 0] = xmin_new
-                verts[verts[:, 0] == xmax, 0] = xmax_new
-
-
 datas = [
     'DBLP-author'         ,
     'DBpedia_locations'   ,
@@ -62,12 +23,53 @@ datas = [
     'UCforum'             ,
     'Unicode'             ,
 ]
+
+# Adjust the widths of a seaborn-generated boxplot.
+def adjust_boxplots(g, width_fac=1, shift_dist=0):
+
+    # Iterate through Axes instances
+    for ax in g.axes:
+        # Iterate through axes artists
+        for c in ax.get_children():
+
+            # Determine the type of the artist
+            c_type = 'Patch'   if isinstance(c, plt_patches.PathPatch) \
+                else 'Line2D'  if isinstance(c, plt_lines.Line2D) \
+                else 'Other'
+
+            # Search for PathPatches and Line2Ds
+            if c_type != 'Other':
+
+                # Get current width of box:
+                p = c.get_path()
+                verts = p.vertices[:-1] if c_type == 'Patch' else p.vertices
+                xmin  = np.min(verts[:, 0])
+                xmax  = np.max(verts[:, 0])
+                xmid  = 0.5 * (xmin + xmax)
+                xhalf = 0.5 * (xmax - xmin)
+
+                # Patch : set new width of box
+                # Line2D: set new width of maximum & minimum & median lines
+                shift_x  = shift_dist if xmid - round(xmid) > 0 else -shift_dist
+                xmin_new = xmid - width_fac * xhalf + shift_x
+                xmax_new = xmid + width_fac * xhalf + shift_x
+                verts[verts[:, 0] == xmin, 0] = xmin_new
+                verts[verts[:, 0] == xmax, 0] = xmax_new
+
+# Create a figure and axis for plotting
 fig, ax = plt.subplots(figsize=(22.5, 6))
 
+# Initialize lists to store data standard deviations
 data_stdevs_WS   = []
 data_stdevs_noWS = []
+
+# Create an empty DataFrame for data storage
 df = pd.DataFrame(columns=['dataset', 'clock', 'work_stealing'])
+
+# Iterate through each dataset
 for data in datas:
+
+    # Open and read the result file for work stealing
     ori_br = open("result/{}_cuMBE_variance".format(data), "r")
     lines = ori_br.readlines()
     del lines[  :5]
@@ -77,11 +79,16 @@ for data in datas:
     for num in res:
         sum += num/len(res)
 
+    # Calculate and store the standard deviation
     data_stdevs_WS.append(np.std(res)/sum)
+
+    # Append data to the DataFrame
     for line in res:
         df = df.append({'dataset':'{}'.format(data), 'clock':line/sum, 'work_stealing': 'WS'}, ignore_index=True)
 
+# Repeat the same process for datasets without work stealing
 for data in datas:
+
     ori_br = open("result/{}_noWS_variance".format(data), "r")
     lines = ori_br.readlines()
     del lines[  :5]
@@ -92,26 +99,35 @@ for data in datas:
         sum += num/len(res)
 
     data_stdevs_noWS.append(np.std(res)/sum)
+
     for line in res:
         df = df.append({'dataset':'{}'.format(data), 'clock':line/sum, 'work_stealing': 'noWS'}, ignore_index=True)
 
+# Print the standard deviation range
 print('[Standard Deviation]')
 print('  WS:', min(data_stdevs_WS), '~', max(data_stdevs_WS))
 print('noWS:', min(data_stdevs_noWS), '~', max(data_stdevs_noWS))
 
+# Create a boxplot using Seaborn
 my_fig = sns.boxplot(x="dataset", y="clock", hue="work_stealing", data=df, showfliers=False, width=0.4, whis=float("inf"))
+
+# Adjust the boxplot widths
 adjust_boxplots(fig, shift_dist=0.075)
 
+# Customize x-axis tick labels
 xticklabel_objects = my_fig.set_xticklabels(my_fig.get_xticklabels(), fontsize=9)
 xticklabel_objects[datas.index('corporate-leadership')].set_fontsize(7.8)
+
+# Add a horizontal grid
 plt.grid(axis='y', linestyle='--', color='grey')
 
+# Set y-axis scale to logarithmic (base 2)
 plt.yscale('log', base=2)
 
+# Customize y-axis tick labels
 y_ticks = [2**i for i in range(math.ceil(np.log2(ax.get_ylim()[0])), math.ceil(np.log2(ax.get_ylim()[1])))]
 plt.xticks()
 y_labels = [f'{tick:.3f}'.rstrip('0').rstrip('.') if tick != int(tick) else f'{int(tick)}' for tick in y_ticks]
-
 plt.yticks(y_ticks, y_labels)
 
 ax.set_xticks(np.arange(-0.5, len(datas)+0.5, 1), minor=True)
--- utils/run.py
+++ utils/run.py
@@ -1,13 +1,9 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--dataDir", type=str, default='./data/bi', help="the path to datasets")
-parser.add_argument("--func", type=str, choices=['variance', 'section'], help="the function of this script")
-args = parser.parse_args()
-
-algos = ['noWS', 'cuMBE'] if args.func == 'variance' else ['noRS', 'noES', 'noWS', 'cuMBE']
-datas = [
+algorithms = ['noRS', 'noES', 'noWS', 'cuMBE']
+algorithms_variance = ['noWS', 'cuMBE']
+datasets = [
     'DBLP-author'             ,
     'DBpedia_locations'       ,
     'Marvel'                  ,
@@ -23,23 +19,44 @@ datas = [
     'Unicode'                 ,
 ]
 
-if args.func == None:
+parser = argparse.ArgumentParser()
+parser.add_argument("--func"   , type=str, choices=['variance', 'section'], help="choose the function of this script")
+parser.add_argument("--algo"   , type=str, choices=algorithms, nargs='+', help="specify MBE algorithms")
+parser.add_argument("--data"   , type=str, choices=datasets, nargs='+', help="specify MBE datasets")
+parser.add_argument("--dataDir", type=str, default='./data/bi', help="specify the path to datasets")
+parser.add_argument("--no_run" , action='store_true', default=False, help="skip MBE execution")
+parser.add_argument("--no_plot", action='store_true', default=False, help="skip plotting")
+args = parser.parse_args()
+
+algos = args.algo if args.algo else algorithms if args.func != 'variance' else algorithms_variance
+datas = args.data if args.data else datasets
+
+print('algorithms:', algos)
+print('datasets:', datas)
+print('dataDir:', args.dataDir)
+
+if args.no_run:
+    pass
+elif args.func == None:
     for data in datas:
-        print('\33[33m------ {} ------\33[0m'.format(data))
+        # print('\33[33m====== {} ======\33[0m'.format(data))
         for algo in algos:
+            print('------ {} ------ {} ------'.format(data, algo))
             os.system('./bin/mbe {}/{}.bi {}'.format(args.dataDir, data, algo))
 elif args.func == 'variance':
     for data in datas:
-        print('\33[33m------ {} ------\33[0m'.format(data))
+        # print('\33[33m====== {} ======\33[0m'.format(data))
         for algo in algos:
+            print('------ {} ------ {} ------'.format(data, algo))
             os.system('./bin/mbe_devariance {}/{}.bi {} >> result/{}_{}_variance'.format(args.dataDir, data, algo, data, algo))
 elif args.func == 'section':
     for data in datas:
-        print('\33[33m------ {} ------\33[0m'.format(data))
+        # print('\33[33m====== {} ======\33[0m'.format(data))
         for algo in algos:
+            print('------ {} ------ {} ------'.format(data, algo))
             os.system('./bin/mbe_desection {}/{}.bi {} >> result/{}_{}_section'.format(args.dataDir, data, algo, data, algo))
 
-if args.func == None:
+if args.no_plot or args.func == None:
     pass
 elif args.func == 'variance':
     os.system('python utils/plot_variance.py')
