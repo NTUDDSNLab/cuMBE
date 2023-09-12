5367dcac611ac9607c8c86d2e4aaec16901dbca6
boshow88 <boshow0122@gmail.com>
2023-09-12 17:39:03 +0800

    Modify CUDA_GLOBAL_EXTENSION kernel

--- src/global_extension.cuh
+++ src/global_extension.cuh
@@ -13,25 +13,27 @@ __global__ void CUDA_GLOBAL_EXTENSION(int *g_u2L, int *g_v2P, int *g_v2Q, int *g
 
     __syncthreads();
     
-    for (int i = threadIdx.x; i < *NUM_L; i += blockDim.x) {
-        g_u2L  [offset_L + i] = u2L  [i];
-        g_L    [offset_L + i] = L    [i];
-        g_L_buf[offset_L + i] = L_buf[i];
+    for (int i = threadIdx.x, g_i; i < *NUM_L; i += blockDim.x) {
+        g_i = offset_L + i;
+        g_u2L  [g_i] = u2L  [i];
+        g_L    [g_i] = L    [i];
+        g_L_buf[g_i] = L_buf[i];
     }
     
-    for (int i = threadIdx.x; i < *NUM_R; i += blockDim.x) {
-        g_v2P    [offset_R + i] = v2P    [i];
-        g_v2Q    [offset_R + i] = v2Q    [i];
-        g_R      [offset_R + i] = R      [i];
-        g_P      [offset_R + i] = P      [i];
-        g_Q      [offset_R + i] = Q      [i];
-        g_x      [offset_R + i] = x      [i];
-        g_L_lp   [offset_R + i] = L_lp   [i];
-        g_R_lp   [offset_R + i] = R_lp   [i];
-        g_P_lp   [offset_R + i] = P_lp   [i];
-        g_Q_lp   [offset_R + i] = Q_lp   [i];
-        g_num_N_u[offset_R + i] = num_N_u[i];
-        g_pre_min[offset_R + i] = pre_min[i];
+    for (int i = threadIdx.x, g_i; i < *NUM_R; i += blockDim.x) {
+        g_i = offset_R + i;
+        g_v2P    [g_i] = v2P    [i];
+        g_v2Q    [g_i] = v2Q    [i];
+        g_R      [g_i] = R      [i];
+        g_P      [g_i] = P      [i];
+        g_Q      [g_i] = Q      [i];
+        g_x      [g_i] = x      [i];
+        g_L_lp   [g_i] = L_lp   [i];
+        g_R_lp   [g_i] = R_lp   [i];
+        g_P_lp   [g_i] = P_lp   [i];
+        g_Q_lp   [g_i] = Q_lp   [i];
+        g_num_N_u[g_i] = num_N_u[i];
+        g_pre_min[g_i] = pre_min[i];
     }
-    
+
 }
